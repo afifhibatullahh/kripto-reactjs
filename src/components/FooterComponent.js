c1738455473ffea87bb822445958cc15c75a2a12
afifhibatullahh <afifhibatullah59@gmail.com>
2021-12-13 12:33:58 +0700

    edit footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -2,10 +2,14 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="container" style={{ marginTop: 200 }}>
+    <div className="container" style={{ marginTop: 200, }}>
       <div className="row d-flex justify-content-center">
-        <p className="text-center" style={{fontSize:36}}>Kriptos</p>
-        <p className="text-center" style={{fontSize:36}}>Grafos</p>
+        <p className="text-center txt-border" style={{ fontSize: 36 }}>
+          Kriptos
+        </p>
+        <p className="text-center" style={{ fontSize: 36 }}>
+          Grafos
+        </p>
         <div className="row d-flex justify-content-center">
           <div className="col-6">
             <hr />
@@ -17,8 +21,15 @@ const Footer = () => {
             Institute of Technology, informatics engineering.
           </p>
         </div>
-        <p className="text-center" style={{fontSize:24}}>Members</p>
-        <p className="text-center">Alfarizi Afif Fajari Nagachi Rizqi </p>
+        <p className="text-center txt-border-white" style={{ fontSize: 24 }}>
+          Members
+        </p>
+        <p className="text-center" style={{marginBottom:150}}>Alfarizi Afif Fajari Nagachi Rizqi </p>
+        <img
+          className="position-absolute end-0"
+          style={{ width: 90,  marginRight: 30, marginTop:120 }}
+          src="./assets/images/squid2.png"
+        />
       </div>
     </div>
   );
